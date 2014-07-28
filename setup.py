bb976e8bb8b1b7fb9948f58b8f8d306cf5b5d5a9
Martin Slabber <martin.slabber@gmail.com>
2014-07-28 11:35:31 +0200

    Initial.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pypelco',
+    version='0.1dev',
+    packages=['pypelco'],
+    license='MIT',
+    long_description=open('README.rst').read(),
+)
